f11e98f4584dd365a5275e8767fde079db044983
andre <andrecabralpires@gmail.com>
2021-06-15 11:21:06 +0100

    lecture

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :restaurants
+  acts_as_token_authenticatable
 end
